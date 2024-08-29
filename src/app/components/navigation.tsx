f0192437efa0bf83cc9c0bc8ed626f23399b9dc0
VeronikaVaraksina1 <plaguemoon@gmail.com>
2024-08-29 12:51:43 +0300

    add pseudoelement (bottom line)

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 export default function Navigation() {
   return (
-    <ul className='flex gap-7'>
-      <li className="leading-tight">
-        <Link href={'/'}>Home</Link>
+    <ul className="flex gap-7">
+      <li className="relative leading-tight pseudoelement-bottom-line">
+        <Link href={'/'}>
+          Home
+        </Link>
       </li>
-      <li className="leading-tight">
+      <li className="relative leading-tight pseudoelement-bottom-line">
         <Link href={'/teachers'}>Teachers</Link>
       </li>
     </ul>
